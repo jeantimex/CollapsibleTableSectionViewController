aaed48ad1d12bc449dd3e5fc42075c7fed2d1085
jeantimex <jean.timex@gmail.com>
2017-07-20 17:48:08 -0700

    Initial setup.

---
+++ CollapsibleTableSectionViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollapsibleTableSectionViewController
+//
+//  Created by Yong Su on 7/20/17.
+//  Copyright © 2017 jeantimex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
