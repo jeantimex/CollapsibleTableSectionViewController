63d4188d99ac104dddd330a71623ad94231738f5
jeantimex <jean.timex@gmail.com>
2017-07-26 21:44:12 -0700

    Clean up the files.

--- CollapsibleTableSectionViewController/CollapsibleTableViewHeader.swift
+++ CollapsibleTableSectionViewController/CollapsibleTableViewHeader.swift
@@ -26,7 +26,6 @@ class CollapsibleTableViewHeader: UITableViewHeaderFooterView {
         // Content View
         contentView.backgroundColor = UIColor(hex: 0x2E3944)
         
-        
         let marginGuide = contentView.layoutMarginsGuide
         
         // Arrow label
@@ -76,4 +75,33 @@ class CollapsibleTableViewHeader: UITableViewHeaderFooterView {
     }
     
 }
+
+extension UIColor {
+    
+    convenience init(hex:Int, alpha:CGFloat = 1.0) {
+        self.init(
+            red:   CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8)  / 255.0,
+            blue:  CGFloat((hex & 0x0000FF) >> 0)  / 255.0,
+            alpha: alpha
+        )
+    }
+    
+}
+
+extension UIView {
+    
+    func rotate(_ toValue: CGFloat, duration: CFTimeInterval = 0.2) {
+        let animation = CABasicAnimation(keyPath: "transform.rotation")
+        
+        animation.toValue = toValue
+        animation.duration = duration
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = kCAFillModeForwards
+        
+        self.layer.add(animation, forKey: nil)
+    }
+    
+}
+
 
