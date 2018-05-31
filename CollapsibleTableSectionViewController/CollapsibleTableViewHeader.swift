a4266c6451d072698e3054201fed751bca732781
Cem Sinan Kaynar <ckaynar@MagiClick.com>
2018-05-31 15:49:18 +0300

    swift 4 support - @objc added

--- CollapsibleTableSectionViewController/CollapsibleTableViewHeader.swift
+++ CollapsibleTableSectionViewController/CollapsibleTableViewHeader.swift
@@ -59,7 +59,7 @@ open class CollapsibleTableViewHeader: UITableViewHeaderFooterView {
     //
     // Trigger toggle section when tapping on the header
     //
-    func tapHeader(_ gestureRecognizer: UITapGestureRecognizer) {
+    @objc func tapHeader(_ gestureRecognizer: UITapGestureRecognizer) {
         guard let cell = gestureRecognizer.view as? CollapsibleTableViewHeader else {
             return
         }
